cca59fd4d2d879daa64403e5eb225ff5d0f7d962
beastsam <41358375+beastsam@users.noreply.github.com>
2018-09-18 14:55:47 +0800

    Create water_mark_manage.h

---
+++ water_mark_manage.h
@@ -0,0 +1,38 @@
+/*
+ * 动态水印管理接口
+ * 作者：高杨
+ * 日期：2018.9.18
+ * 功能：根据用户名，终端机器特征码，文件标题，当前时间等信息生成图片形式的水印
+ * 参数：
+ *     pcUserName[in]    用户名
+ *     aucDevcode[in]    终端机器特征码
+ *     pcTitle[in]       文件标题
+ *     pcCurtime[in]     当前时间
+ *     pucPic[out]       图片形式水印缓冲区
+ *     uisize[out]       记录图片形式水印缓冲区大小
+ * 返回值：
+ *       如成功执行返回0
+ *       若执行失败返回错误代码  
+ * 备注
+ */
+ 
+#ifndef WATER_MARK_MANAGE_H_
+#define WATER_MARK_MANAGE_H_
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define ERROR_PARAM 11               
+#define ERROR_GET_FONT_SIZE 12
+#define ERROR_RENDER_IMAGE 13
+
+#define MAX_USERNAME 256+1
+#define MAX_DEVCODE 16+1
+#define MAX_TITLE 256+1
+#define MAX_TIME 32+1
+#define MAX_BUFFER 564
+ 
+int water_mark_manage(char *pcUserName, char *aucDevcode,char *pcTitle, 
+							char *pcCurtime, char *pucPic, int *uisize);
+							
+ 
+#endif
